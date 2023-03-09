ada05f353efa268e5621d15ba6d07659c92b239b
kud-github <kud@truelink.dk>
2023-03-09 18:33:30 +0200

    Format code

--- src/main/java/ua/kud/neetcode/leetcode/arrays/ContainsDuplicate.java
+++ src/main/java/ua/kud/neetcode/leetcode/arrays/ContainsDuplicate.java
@@ -51,7 +51,7 @@ public class ContainsDuplicate {
 	}
 
 	public boolean containsDuplicateSolveByMap(int[] nums) {
-		Set<Integer> set  = new HashSet<Integer>();
+		Set<Integer> set = new HashSet<Integer>();
 		for (int i = 0; i < nums.length; i++) {
 			if (!set.add(nums[i])) {
 				return true;
